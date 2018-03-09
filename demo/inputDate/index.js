88fb5df438e958b75ab06c1dcc794359742be768
ucdbook <2681700197@qq.com>
2018-03-09 14:59:33 +0800

    update datePicker demo

--- demo/inputDate/index.js
+++ demo/inputDate/index.js
@@ -1,8 +1,33 @@
 import formInputDate from "./formInputDate.html";
+import timePicker from "./timePicker.html";
+import weekPicker from "./weekPicker.html";
+import datePickerRange from "./datePickerRange.html";
 
-export default {
-	title: "InputDate",
+export default [{
+	title: "datePicker",
+	parentTitle: "DatePickers",
 	author: "tianyanrong",
 	html: [formInputDate],
 	isCode: true
-}
+},
+{
+	title: "timePicker",
+	parentTitle: "DatePickers",
+	author: "tianyanrong",
+	html: [timePicker],
+	isCode: true
+},
+{
+	title: "weekPicker",
+	parentTitle: "DatePickers",
+	author: "tianyanrong",
+	html: [weekPicker],
+	isCode: true
+},
+{
+	title: "datePickerRange",
+	parentTitle: "DatePickers",
+	author: "tianyanrong",
+	html: [datePickerRange],
+	isCode: true
+}]
