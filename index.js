8cc09b9bd9a418acea5cba9e446dea6843ff463a
chengle123 <314737853@qq.com>
2018-03-13 17:37:15 +0800

    增加环形百分比

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ import selectDropdown from './src/selectDropdown/selectDropdown';
 import imageUpload from './src/imageUpload/imageUpload';
 
 import numberSelect from './src/numberSelect/numberSelect';
+import circleSchedule from './src/circleSchedule/circleSchedule';
 
 
 export default {
@@ -94,4 +95,5 @@ export default {
   selectDropdown,
   'imageUpload/imageUpload': imageUpload,
   numberSelect,
+  'circleSchedule/circleSchedule': circleSchedule
 };
